25cf2ae8ae88183e31a50b5b2c37ce229d6a1712
ZHEKO <fr.artuso@duocuc.cl>
2022-08-15 02:07:24 -0400

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'cuenta-cliente',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
 
-  constructor() { }
+export class LoginPage {
+  @ViewChild(IonModal) modal: IonModal;
 
-  ngOnInit() {
+  message = '';
+  name: string;
+
+  cancelar() {
+    this.modal.dismiss(null, 'cancelar');
   }
 
+  confirmar() {
+    this.modal.dismiss(this.name, 'confirmar');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirmar') {
+      this.message = `Hola, ${ev.detail.data}!`;
+    }
+  }
 }
