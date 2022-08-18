930e3a04f9e02e39d7bea76fdb9792b207eb6f67
ZHEKO <fr.artuso@duocuc.cl>
2022-08-17 21:19:22 -0400

    API de mapa añadido

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let google;
+
+interface Marker {
+  position: {
+    lat: number;
+    lng: number;
+  };
+  title: string;
+}
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
+export class MapaPage implements OnInit{
 
-  constructor() { }
+  map = null;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit(){
+      this.loadMap();
+  }
+
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = {lat: -33.36326318588252, lng: -70.67801166481883};
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 13
+    });
+
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      mapEle.classList.add('show-map');
+      const market = {
+        position: {
+          lat: -33.36326318588252,
+          lng: -70.67801166481883,
+        },
+        title: 'Duoc UC: Sede Plaza Norte'
+      };
+      this.addMarker(market);
+    });
+  }
+
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
+    });
   }
 
 }
